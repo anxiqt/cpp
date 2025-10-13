fd689c905312604914bdff70ca655582709ccd08
Justin Lutz <jlutz234@proton.me>
2025-10-13 14:52:33 -0500

    first update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+
+double square(double x)
+{
+
+    return x*x;
+}
+
+void print_square(double x)
+{
+    std::cout << "the square of " << x << " is " << square(x) << std::endl;
+}
+
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+    print_square(1.234);
+    
     return 0;
 }
